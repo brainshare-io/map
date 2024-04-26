c617159eee973cdc44241c25bc6de8d2732036d0
Zachary King <zak@delfina.com>
2024-04-26 09:59:03 -0700

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+// TODO set up the CLI to install dependencies and make a nice environment for
+// local dev
+
+// TODO can publishing to npm be optional? Would make downstream builds way
+// faster of course. Maybe github OK for private and npm for public? Or just do
+// heavy caching?
+
+export default function Map({ data }: { data: any }) {
+  return <>Map</>;
+}
